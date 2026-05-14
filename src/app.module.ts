4b2082d6e7065aed95bdf4db0d81b55810f3625f
j0s3salg4do <j.salgadop@uniandes.edu.co>
2026-05-13 22:02:47 -0500

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
@@ -13,16 +14,21 @@ import { Role } from './roles/entities/role.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'preparcial_db',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [User, Role],
-      synchronize: true,
+      synchronize: false,
     }),
+
     AuthModule,
     RolesModule,
     UsersModule,
